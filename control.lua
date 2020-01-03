f7a487a99d630080330ad5bb7f9eeb06f83bc77b
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-01-03 15:06:03 -0700

    blah

--- control.lua
+++ control.lua
@@ -62,14 +62,6 @@ script.on_event(defines.events.on_player_created, function(event)
 	local r = global.chart_distance or 200
 	player.force.chart(player.surface, {{player.position.x - r, player.position.y - r}, {player.position.x + r, player.position.y + r}})
 
-	if not global.skip_intro then
-		if game.is_multiplayer() then
-			player.print({"msg-intro"})
-		else
-			game.show_message_dialog{text = {"msg-intro"}}
-		end
-	end
-
 	silo_script.on_event(event)
 	player.force.friendly_fire = false --friendly fire
 	
