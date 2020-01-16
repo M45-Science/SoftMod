9205339da20f93497996248dbe12fe11532e69cb
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-01-16 13:21:19 -0700

    x/y mouse

--- control.lua
+++ control.lua
@@ -837,6 +837,10 @@ script.on_event(
     function(event)
         local toremove
 
+        for _, player in pairs(game.connected_players) do
+            player.surface.create_entity{name="flying-text", position=player.position, text=string.format("%d,%d",player.cursor.position.x + 10, player.cursor.position.y + 10), color={r = 0, g = 255, b = 255}, time_to_live=1}
+        end
+
         if (not global.last_s_tick) then
             global.last_s_tick = 0
         end
