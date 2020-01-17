246b11653a7afc611418802390f6a103ca507132
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-01-16 17:48:43 -0700

    fix

--- control.lua
+++ control.lua
@@ -73,7 +73,7 @@ end
 local function sandbox_mode(player)
     return
 
-    if player ~= nil then
+    if 1 == 2 and player ~= nil then
         player.cheat_mode = true
         player.surface.always_day = true
         player.force.laboratory_speed_modifier = 1
