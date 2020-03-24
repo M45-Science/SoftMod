3919c036b9af2168c75d2860258f97b0f03ea0a8
Distortions81 <33408758+Distortions81@users.noreply.github.com>
2020-03-24 13:26:02 -0600

    Update control.lua

--- control.lua
+++ control.lua
@@ -399,7 +399,7 @@ end
 local function clean_surfaces()
     message_all("Cleaning map, game will freeze for some time...")
 
-    local radius = 50
+    local radius = 20
     local keep_paving = true
 
     -- Get list of possible paving
