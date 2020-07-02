6723e058b34b6192a59f49bd54bcf1d5ccf25f7f
Distortions81 <carlotto81@gmail.com>
2020-07-02 04:52:41 -0600

    fix

--- control.lua
+++ control.lua
@@ -2040,8 +2040,6 @@ script.on_event(
             player.gui.top.add {type = "textfield", name = "discord"}
             player.gui.top.discord.text = "discord.gg/Ps2jnm7"
             player.gui.top.discord.tooltip = "Select with mouse and press control-c to copy!"
-            player.gui.top.discord.text.read_only = true
-            player.gui.top.discord.text.selectable = true
 
         end
 
@@ -2447,8 +2445,8 @@ script.on_nth_tick(
             --Repair discord info
             if player and player.valid and player.gui and player.gui.top and player.gui.top.discord then
                 player.gui.top.discord.text = "discord.gg/Ps2jnm7"
-                player.gui.top.discord.text.read_only = true
-                player.gui.top.discord.text.selectable = true
+                player.gui.top.discord.read_only = true
+                player.gui.top.discord.selectable = true
             end
         end
     end
