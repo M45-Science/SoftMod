6a9fd02d2631d66240fe8eeb330ca8bad4083727
Distortions81 <carlotto81@gmail.com>
2020-10-14 18:32:05 -0600

    oh

--- speed.control.lua
+++ speed.control.lua
@@ -2118,7 +2118,7 @@ script.on_nth_tick(
 
             for _, player in pairs(game.connected_players) do
                 if player and player.valid and player.gui and player.gui.top and player.gui.top.gtimer then
-                    player.gui.top.gtimer = ts
+                    player.gui.top.gtimer.text = ts
                 end
             end
         else
