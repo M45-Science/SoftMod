7086cb1e39f7277a51a20cea7e4bcd7ea6b014ce
Distortions <33408758+Distortions81@users.noreply.github.com>
2020-11-20 04:54:23 -0700

    Delete copy.sh

--- copy.sh
+++
@@ -1,8 +0,0 @@
-cp -f control.lua ../../fact-a/saves/
-cp -f control.lua ../../fact-b/saves/
-cp -f control.lua ../../fact-c/saves/
-cp -f control.lua ../../fact-d/saves/
-
-cp -f control.lua ../../fact-ra/saves/control.lua
-cp -f control.lua ../../fact-rb/saves/control.lua
-cp -f control.lua ../../fact-rc/saves/control.lua
