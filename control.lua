a64ac5b1ae1c1ac2f59d15bef522da776fec6190
Distortions81 <carlotto81@gmail.com>
2020-12-12 01:12:49 -0700

    added map tag text to log

--- control.lua
+++ control.lua
@@ -1,4 +1,4 @@
---v505-121220200106a
+--v505-121220200110a
 --Carl Frank Otto III (aka Distortions864)
 --carlotto81@gmail.com
 
@@ -1842,7 +1842,7 @@ script.on_event(
             local player = game.players[event.player_index]
 
             if player and player.valid and event.tag then
-                console_print(player.name .. " placed a map tag at [gps=" .. event.tag.position.x .. "," .. event.tag.position.y .. "] called: " .. event.tag.object_name)
+                console_print(player.name .. " placed a map tag at [gps=" .. event.tag.position.x .. "," .. event.tag.position.y .. "] called: " .. event.tag.text)
             end
         end
     end
@@ -1855,7 +1855,7 @@ script.on_event(
         if event and event.player_index then
             local player = game.players[event.player_index]
             if player and player.valid and event.tag then
-                console_print(player.name .. " edited a map tag at [gps=" .. event.tag.position.x  .. "," .. event.tag.position.y  .. "] called: " .. event.tag.object_name)
+                console_print(player.name .. " edited a map tag at [gps=" .. event.tag.position.x  .. "," .. event.tag.position.y  .. "] called: " .. event.tag.text)
             end
         end
     end
@@ -1869,7 +1869,7 @@ script.on_event(
             local player = game.players[event.player_index]
 
             if player and player.valid and event.tag then
-                console_print(player.name .. " deleted a map tag at [gps=" .. event.tag.position.x  .. "," .. event.tag.position.y .. "] called: " .. event.tag.object_name)
+                console_print(player.name .. " deleted a map tag at [gps=" .. event.tag.position.x  .. "," .. event.tag.position.y .. "] called: " .. event.tag.text)
             end
         end
     end
