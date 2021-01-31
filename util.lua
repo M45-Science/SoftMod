0d98af278980254751eed591f571087fee4b9a2a
Distortions81 <carlotto81@gmail.com>
2021-01-31 00:27:40 -0700

    steal dist_to

--- util.lua
+++ util.lua
@@ -38,6 +38,18 @@ function console_print(message)
     end
   end
 
+
+  --Calculate distance between two points
+function dist_to(pos_a, pos_b)
+  if pos_a and pos_b and pos_a.x and pos_a.y and pos_b.x and pos_b.y then
+    local axbx = pos_a.x - pos_b.x
+    local ayby = pos_a.y - pos_b.y
+    return (axbx * axbx + ayby * ayby) ^ 0.5
+  else
+    return 10000000
+  end
+end
+
 --Show players online to a player
 function show_players(victim)
   local buf = ""
