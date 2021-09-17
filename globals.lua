05fa258faa5ce7f76c513ffea1dd5d097de3e3ef
Distortions <33408758+Distortions81@users.noreply.github.com>
2021-09-17 11:09:21 -0600

    version

--- globals.lua
+++ globals.lua
@@ -3,7 +3,7 @@
 
 --Create globals, if needed
 function create_myglobals()
-  global.svers = "563-08012021-1059a-exp"
+  global.svers = "564-09172021-1109-exp"
 
   if not global.restrict == nil then
     global.restrict = true
