0533aed7366aa9c37357eab444fbbc62b7daddf6
Distortions <33408758+Distortions81@users.noreply.github.com>
2021-11-18 15:09:07 -0700

    add newline to json

--- antigrief.lua
+++ antigrief.lua
@@ -28,7 +28,9 @@ function on_robot_built_entity(event)
 
           --Map data export
           local olog = {tick = game.tick, creator = bot.name, name = obj.name, type = obj.type, position = obj.position, direction = obj.direction, surface = obj.surface.name, force = obj.force.name, rotated = false, mined = false, robot = true}
-          game.write_file("mapdata.dat", game.table_to_json(olog), true, 0)
+          game.write_file("mapdata.dat", game.table_to_json(olog).."\n", true, 0)
+        else
+          --console_print("bot +"..obj.name.." [gps="..obj.position.x..","..obj.position.y.."] ", obj.ghost_name)
         end
       end
     else
@@ -66,10 +68,12 @@ function on_built_entity(event)
 
           --Map data export
           local olog = {tick = game.tick, creator = player.name, name = obj.name, type = obj.type, position = obj.position, direction = obj.direction, surface = obj.surface.name, force = obj.force.name, rotated = false, mined = false, robot = false}
-          game.write_file("mapdata.dat", game.table_to_json(olog), true, 0)
+          game.write_file("mapdata.dat", game.table_to_json(olog).."\n", true, 0)
 
         --Save to db
         --table.insert(global.objmap, olog)
+        else
+          console_print(player.name.." +"..obj.name.." [gps="..obj.position.x..","..obj.position.y.."] " .. obj.ghost_name)
         end
       end
     else
@@ -98,7 +102,9 @@ function on_pre_player_mined_item(event)
 
           --Map data export
           local olog = {tick = game.tick, creator = player.name, name = obj.name, type = obj.type, position = obj.position, direction = obj.direction, surface = obj.surface.name, force = obj.force.name, rotated = false, mined = true, robot = false}
-          game.write_file("mapdata.dat", game.table_to_json(olog), true, 0)
+          game.write_file("mapdata.dat", game.table_to_json(olog).."\n", true, 0)
+        else
+          console_print(player.name .. " -" .. obj.name .. " [gps=" .. obj.position.x .. "," .. obj.position.y .. "] " .. obj.ghost_name)
         end
       end
     else
@@ -126,7 +132,9 @@ function on_robot_pre_mined(event)
 
             --Map data export
             local olog = {tick = game.tick, creator = bot.name, name = obj.name, type = obj.type, position = obj.position, direction = obj.direction, surface = obj.surface.name, force = obj.force.name, rotated = false, mined = true, robot = true}
-            game.write_file("mapdata.dat", game.table_to_json(olog), true, 0)
+            game.write_file("mapdata.dat", game.table_to_json(olog).."\n", true, 0)
+          else
+            --console_print("robot " .. " -" .. obj.name .. " [gps=" .. obj.position.x .. "," .. obj.position.y .. "] " .. obj.ghost_name)
           end
         end
       else
@@ -159,7 +167,9 @@ function on_player_rotated_entity(event)
 
             --Map data export
             local olog = {tick = game.tick, creator = player.name, name = obj.name, type = obj.type, position = obj.position, direction = obj.direction, surface = obj.surface.name, force = obj.force.name, rotated = true, mined = false, robot = false}
-            game.write_file("mapdata.dat", game.table_to_json(olog), true, 0)
+            game.write_file("mapdata.dat", game.table_to_json(olog).."\n", true, 0)
+          else
+            console_print(player.name .. " *" .. obj.name .. " [gps=" .. obj.position.x .. "," .. obj.position.y .. "] " .. obj.ghost_name)
           end
         end
       else
