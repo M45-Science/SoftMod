848a11a9d3022539a42dce56217a97c12ef32e34
Distortions81 <carlotto81@gmail.com>
2021-12-22 22:27:00 -0700

    version

--- globals.lua
+++ globals.lua
@@ -3,7 +3,7 @@
 
 --Create globals, if needed
 function create_myglobals()
-  global.svers = "576-11202021-1137-exp"
+  global.svers = "577-12222021-1025p-exp"
 
   if not global.levelstart then
     global.levelstart = true
