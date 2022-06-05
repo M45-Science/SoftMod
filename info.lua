2590ce523921866d6668e116c40c0f4a3159c550
Distortions81 <carlotto81@gmail.com>
2022-06-04 19:01:34 -0600

    larger text

--- info.lua
+++ info.lua
@@ -297,7 +297,7 @@ function make_m45_info_window(player)
       }
       tab1_info_top.add {
         type = "label",
-        caption = "[font=default-large]ULTIMATE DEATHWORLD: NO FISH! JUST ONE LIFE![/font]"
+        caption = "[entity=character]  [color=yellow][font=default-large-bold]NULTIMATE DEATHWORLD: NO FISH! JUST ONE LIFE!![/font][/color]"
       }
       tab1_info_top.add {
         type = "label",
