4d7fa10504265703b5b25196c62acae17620cd7b
Distortions81 <carlotto81@gmail.com>
2022-12-17 13:34:50 -0700

    version

--- globals.lua
+++ globals.lua
@@ -4,7 +4,7 @@
 -- License: MPL 2.0
 -- Create globals, if needed
 function create_myglobals()
-    global.svers = "610-12.14.2022-0606p"
+    global.svers = "610-12.17.2022-0134p"
 
     -- Adjust look
     game.surfaces[1].show_clouds = false
