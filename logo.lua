4455ca7bc5dfce4e7cfdf7bc68a21b9b34d21f97
Distortions81 <carlotto81@gmail.com>
2024-04-06 15:45:51 -0600

    Update icon, spawn pad, etc

--- logo.lua
+++ logo.lua
@@ -38,7 +38,7 @@ function dodrawlogo()
             -- Set drawn flag
             global.drawlogo = true
             global.m45logo = rendering.draw_sprite {
-                sprite = "file/img/world/m45-pad-v5.png",
+                sprite = "file/img/world/m45-pad-v6.png",
                 render_layer = "floor",
                 target = cpos,
                 x_scale = 0.5,
